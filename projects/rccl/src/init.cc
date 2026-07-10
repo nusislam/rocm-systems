e7500413ddc4333aa3abc9cbb6f4bbc32cc64292
Nusrat Islam <Nusrat.Islam@amd.com>
2026-07-10 09:31:33 -0500

    fix setting comm->ddaEnable based on env

--- projects/rccl/src/init.cc
+++ projects/rccl/src/init.cc
@@ -673,6 +673,7 @@ static ncclResult_t commAlloc(struct ncclComm* comm, struct ncclComm* parent, in
   comm->ddaFabricBarrierState = nullptr;
   comm->ddaFabricMemHandler = nullptr;
   comm->ddaFabricMaxBlocks = 0;
+  comm->ddaEnable = (int)rcclParamDdaEnable();
 
   comm->rank = rank;
   comm->nRanks = ndev;
